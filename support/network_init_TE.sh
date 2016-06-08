e0e9a26a05b1091878a54888e1ddfbe93fb2efe0
rgrezo <rudolf.grezo@gmail.com>
2016-06-08 10:04:52 +0200

    Network init with new topology for traffic engineering. New startup script for traffic engineering.

---
+++ support/network_init_TE.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#
+# Network topology initiation script
+# If no parameters is given, topology is configured for BTS setup (use of eth2)
+# If any parameter is given, extra interface towards tcpreplay (simulation BTS is configures)
+
+
+topologySetup="bts"
+if [ $# -eq 0 ]; then
+   topologySetup="bts"
+else
+   topologySetup="tcpreplay"
+fi
+  
+ip link add eth2 type veth peer name corea2
+ip link add eth3 type veth peer name corec3
+ip link add corea3 type veth peer name coreb1
+ip link add corea4 type veth peer name cored1
+ip link add coreb2 type veth peer name cored2
+ip link add corec1 type veth peer name coreb3
+ip link add corec2 type veth peer name coree2
+ip link add cored3 type veth peer name coree1
+ip link add corea5 type veth peer name coref1
+ip link add corec3 type veth peer name coref2
+
+#ifconfig vgsn0 192.168.27.2/24
+#ifconfig vgsn0 up
+
+#ifconfig internet0 172.20.255.254/16
+#ifconfig internet0 up
+
+ethtool --offload vgsn0 rx off tx off
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+service dnsmasq restart
+
+
+tcpreplayInterfaces=( bss0 corea1 )
+btsInterfaces=(eth2)
+commonInterfaces=( corea3 corea4 corea5 coreb2 corec1 corec2 corec3 cored3 coreb1 cored1 cored2 coreb3 coree2 coree1 )
+
+# When UnifyCore is being run with tcpDump, one link needs to be added (this is used instead of eth2)
+if [ "$topologySetup" == "tcpreplay" ]; then
+   ip link add bss0 type veth peer name corea1
+   echo "**** Configuring tcpreplay setup ****"
+   interfaces=( "${tcpreplayInterfaces[@]}" "${commonInterfaces[@]}" )
+else
+   echo "**** Configuring BTS setup ****"
+   interfaces=( "${btsInterfaces[@]}" "${commonInterfaces[@]}" )
+fi
+
+#echo ${interfaces[@]}
+for INT in ${interfaces[@]}; do
+	ip link set $INT mtu 1500
+done
+ip link set eth2 mtu 1500
+#ip link set eth2 mtu 1600
+#XXX: toto mozno nie je treba?
+#ip link set internet0 mtu 1200
---
+++ support/sdn_TE.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# start-up script for the whole unifycore topology (both forwarders and controllers)
+
+
+if [[ $EUID -ne 0 ]]; then
+    echo 'Dont forget to use sudo ;-)'
+    exit 1
+fi
+
+FWD_ROOT="/root/ofsoftswitch13"
+#CNT_SCRIPT="/root/ryu/ryu/app/magic.py"
+
+CONTROLLER="tcp:127.0.0.1:6633"
+
+#VALGRIND="valgrind --suppressions=/root/valgrind.suppress --leak-check=yes "
+VALGRIND="valgrind --log-file=/tmp/valgrind.log --tool=memcheck --leak-check=yes "
+
+function start_fwd() {
+	SOCKET="unix:/tmp/dp$1.socket"
+	$FWD_ROOT/udatapath/ofdatapath -i "$3" -d "$2" "p$SOCKET" -v > /tmp/dp$1.log 2>&1 &
+	$FWD_ROOT/secchan/ofprotocol "$SOCKET" "$CONTROLLER" --fail=closed  > /tmp/of$1.log 2>&1 &
+}
+
+function debug_fwd() {
+	SOCKET="unix:/tmp/dp$1.socket"
+	$FWD_ROOT/secchan/ofprotocol "$SOCKET" "$CONTROLLER" --fail=closed  > /tmp/of$1.log 2>&1 &
+	gdb --args $FWD_ROOT/udatapath/ofdatapath -i "$3" -d "$2" "p$SOCKET" -v 
+}
+
+function valgrind_fwd() {
+	SOCKET="unix:/tmp/dp$1.socket"
+	$FWD_ROOT/secchan/ofprotocol "$SOCKET" "$CONTROLLER" --fail=closed  > /tmp/of$1.log 2>&1 &
+	$VALGRIND $FWD_ROOT/udatapath/ofdatapath -i "$3" -d "$2" "p$SOCKET" -v 
+}
+
+function start_cnt() {
+	#screen -d -m -S "controller" ryu-manager $CNT_SCRIPT --observe-links --verbose > /tmp/controller.log 2>&1
+	ryu-manager $CNT_SCRIPT --observe-links --verbose > /tmp/controller.log 2>&1 &
+}
+
+function do_start() {
+  # big topology from the project documentation (and README)
+	#sleep 0.5
+	start_fwd a 00000000000a 'eth2,corea3,corea4,corea5'
+	#sleep 0.5
+	start_fwd b 00000000000b 'coreb1,coreb2,coreb3'
+	#sleep 0.5
+	start_fwd c 00000000000c 'corec1,corec2,eth3,corec3'
+	#$sleep 0.5
+	start_fwd d 00000000000d 'cored1,cored2,cored3'
+	#sleep 0.5
+	start_fwd e 00000000000e 'coree1,coree2'
+	#start_fwd c 00000000000c 'corec1,corec2,corec3'
+	start_fwd f 00000000000f 'coref1,coref2'
+
+	sleep 0.5
+
+	sleep 0.5
+	
+	start_cnt	
+	sleep 0.5
+
+	#valgrind_fwd a 00000000000a 'eth2,corea2,corea3,corea4'
+	#debug_fwd a 00000000000a 'eth2,corea2,corea3,corea4'
+}
+
+function do_stop() {
+	# forwarder
+	killall ofprotocol
+	killall ofdatapath
+
+	# and controller
+	killall ryu-manager
+}
+
+case $1 in
+	start)
+		do_start
+		;;
+	stop)
+		do_stop
+		;;
+	restart)
+		do_stop
+		do_start
+		;;
+esac
+
